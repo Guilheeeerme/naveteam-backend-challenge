e4858ffd5939276e232a9b6e1e569916311dfdb5
Guilheeeerme <guilherme_moura.10222@outlook.com>
2021-04-08 11:11:07 -0300

    chore: date conversion

---
+++ src/utils/convertDate.js
@@ -0,0 +1,8 @@
+function convertDate(data) {
+  const date = new Date(data);
+  const mnth = ("0" + (date.getMonth() + 1)).slice(-2);
+  const day = ("0" + (date.getDate() + 1)).slice(-2);
+
+  return [date.getFullYear(), mnth, day].join("-");
+}
+export default convertDate;
